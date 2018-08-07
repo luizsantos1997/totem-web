d00240179a7e55908ab502d550cb0b38af3dcc7a
Fabricio Carvalhal <fabcarvalhal@gmail.com>
2018-08-07 02:01:11 -0300

    diretiva adicionada para um spinner de loading

---
+++ app/scripts/directives.js
@@ -0,0 +1,18 @@
+app.directive('loading', ['$http', function ($http) {
+    return {
+      restrict: 'E',
+      templateUrl: './templates/loading.tpl.html',
+      link: function (scope, element, attrs) {
+        scope.isLoading = function () {
+          return $http.pendingRequests.length > 0;
+        };
+        scope.$watch(scope.isLoading, function (value) {
+          if (value) {
+            element.removeClass('ng-hide');
+          } else {
+            element.addClass('ng-hide');
+          }
+        });
+      }
+    };
+}]);
